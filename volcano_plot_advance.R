fcae657198a143402db4c33fa45fb6b3abe741c5
L. Mingkai <704021302@qq.com>
2025-03-23 22:52:19 +0800

    Update volcano_plot_advance.R

--- volcano_plot_advance.R
+++ volcano_plot_advance.R
@@ -23,22 +23,32 @@ if (length(unique(data$group)) == 2) {
   palette <- c("#2f5688", "gray", "#CC0000")
 }
 
+# 打开 PDF 文件输出
+pdf("C:/Users/Lamarck/Desktop/Volcano_Plot.pdf", width = 6, height = 6)
+
 # 绘图
-p <- ggscatter(data, x = "logFC", y = "log10_pvalue",
-               color = "group", palette = palette,
-               size = 1.5, label = data$Label, 
-               font.label = 10, repel = TRUE,
-               title = "Volcano Plot",
-               xlab = "log2(Fold Change)", ylab = "-log10(P.Value)") +
+p <- ggscatter(
+  data, x = "logFC", y = "log10_pvalue",
+  color = "group", palette = palette,
+  size = 1.5, label = data$Label,
+  font.label = 10, repel = TRUE,
+  title = "Volcano Plot",
+  xlab = "log2(Fold Change)", ylab = "-log10(P.Value)"
+) +
   geom_vline(xintercept = c(-1, 1), linetype = "dashed") +
   geom_hline(yintercept = -log10(0.05), linetype = "dashed") +
   theme_classic() +  # 可替换为 theme_classic() 或 theme_minimal()
-  theme(legend.title = element_blank(),
-        legend.justification = c(0.1, 0.97),
-        legend.text = element_text(size = 12),
-        plot.title = element_text(face = "bold", size = 16, hjust = 0.5),
-        axis.title = element_text(face = "bold", size = 14),
-        plot.background = element_blank())
+  theme(
+    legend.title = element_blank(),
+    legend.justification = c(0.1, 0.97),
+    legend.text = element_text(size = 12),
+    plot.title = element_text(face = "bold", size = 16, hjust = 0.5),
+    axis.title = element_text(face = "bold", size = 14),
+    plot.background = element_blank()
+  )
 
-# 将图形显示在 RStudio 的 Plot 面板
-p
+# 输出绘图到 PDF
+print(p)
+
+# 关闭 PDF 设备
+dev.off()
